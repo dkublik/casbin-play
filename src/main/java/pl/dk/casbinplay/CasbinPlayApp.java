40d3309e990f9766f8f0462e2dafc7bb8db5e2f9
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2020-10-09 16:59:40 +0200

    spock

--- src/main/java/pl/dk/casbinplay/CasbinPlayApp.java
+++ src/main/java/pl/dk/casbinplay/CasbinPlayApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasbinPlayApplication {
+public class CasbinPlayApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CasbinPlayApplication.class, args);
+		SpringApplication.run(CasbinPlayApp.class, args);
 	}
 
 }
---
+++ src/test/groovy/pl/dk/casbinplay/CasbinPlaySpec.groovy
@@ -0,0 +1,12 @@
+package pl.dk.casbinplay
+
+import spock.lang.Specification
+
+class CasbinPlaySpec extends Specification {
+
+    def "should work"() {
+        expect:
+        1 == 1
+    }
+
+}
