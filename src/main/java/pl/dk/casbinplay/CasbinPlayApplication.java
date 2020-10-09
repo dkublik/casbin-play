410b453e87439bacd41584caa21d1c8d155d70f4
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2020-10-09 16:18:20 +0200

    init

---
+++ src/main/java/pl/dk/casbinplay/CasbinPlayApplication.java
@@ -0,0 +1,13 @@
+package pl.dk.casbinplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasbinPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasbinPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dk/casbinplay/CasbinPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dk.casbinplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasbinPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
