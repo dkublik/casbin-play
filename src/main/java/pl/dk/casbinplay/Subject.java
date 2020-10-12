b9a460dba6bef847a813d8d72a88d3ad5df35174
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2020-10-12 22:57:19 +0200

    simple casbin model

---
+++ src/main/java/pl/dk/casbinplay/Subject.java
@@ -0,0 +1,11 @@
+package pl.dk.casbinplay;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Subject {
+    String login;
+    Integer age;
+}
--- src/test/groovy/pl/dk/casbinplay/CasbinPlaySpec.groovy
+++ src/test/groovy/pl/dk/casbinplay/CasbinPlaySpec.groovy
@@ -1,12 +1,21 @@
 package pl.dk.casbinplay
 
+import org.casbin.jcasbin.main.Enforcer
 import spock.lang.Specification
 
 class CasbinPlaySpec extends Specification {
 
     def "should work"() {
+        given:
+        String modelPath = this.class.classLoader.getResource('security/model.conf').path
+        String policyPath = this.class.classLoader.getResource('security/policy.csv').path
+        Enforcer enforcer = new Enforcer(modelPath, policyPath)
+        Subject alice = new Subject('Alice', 21)
+        String obj = "/data1"
+        String act = "read"
+
         expect:
-        1 == 1
+        enforcer.enforce(alice, obj, act) == true
     }
 
 }
